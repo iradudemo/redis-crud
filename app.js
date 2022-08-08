56818790a1dfe81a3944aacb2ad73b8c0547e295
Jean Claude <demo.claude15@gmail.com>
2022-08-08 06:02:44 +0200

    simple crud

--- app.js
+++ app.js
@@ -1,15 +1,65 @@
-const express = require("express");
+var express = require("express");
+var bodyParser = require("body-parser");
+var config = require("./config/config");
+var appDao = require("./controller/controller");
+var app = express();
 
-const { createClient } = require("redis");
-
-const app = express();
-const PORT = 4001;
-const redisClient = createClient();
-
-redisClient.connect();
+var resp = function (res, data, code, next) {
+  res.status(code).json(data);
+  return next();
+};
 
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`app is listening at port: ${PORT}`);
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Authorization, Origin, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.listen(5000);
+
+console.log("Application is listening on port ", 5000);
+
+app.post("/user/add", function (req, res, next) {
+  var body = req.body;
+
+  appDao.add_user(body, function (response, code) {
+    resp(res, response, code, next);
+  });
+});
+// to be work on
+// app.get("/user", function (req, res, next) {
+//   appDao.get_user(function (response, code) {
+//     resp(res, response, code, next);
+//   });
+// });
+app.get("/user/:id", function (req, res, next) {
+  var param = req.params;
+
+  appDao.get_userId(param, function (response, code) {
+    resp(res, response, code, next);
+  });
+});
+app.put("/user/:id", function (req, res, next) {
+  var id = req.params.id;
+  var param = req.body;
+
+  appDao.update_user(id, param, function (response, code) {
+    resp(res, response, code, next);
+  });
+});
+app.delete("/user/:id", function (req, res, next) {
+  var param = req.params;
+
+  appDao.delete_user(param, function (response, code) {
+    resp(res, response, code, next);
+  });
 });
---
+++ readme.md
@@ -0,0 +1,23 @@
+# node-redis-crud
+
+A simple crud application with redis database
+
+Application is running on http://localhost:2003/
+
+### Services to call
+
+#### _Add User_
+
+[POST] http://localhost:2003/user/add
+
+#### _Get User By ID_
+
+[GET] http://localhost:2003/user/:id
+
+#### _Update User_
+
+[PUT] http://localhost:2003/user/:id
+
+#### _Delete User_
+
+[DELETE] http://localhost:2003/user/:id
